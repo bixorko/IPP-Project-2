b1806d4b5e8b596526c3cdc9ee60348133f642f4
bixorko <peter.vinarcik99@gmail.com>
2020-03-27 23:27:16 +0100

    many operations done + LF GF TF

--- interpret.py
+++ interpret.py
@@ -11,6 +11,8 @@ stringregex = "^string@(\S)+$"
 boolregex = "^bool@(true|false)$"
 intregex = "^int@[+|-]?[0-9]+"
 var = {}
+varLF = {}
+varTF = {}
 labels = {}
 localframe = False
 tempframe = False
@@ -123,9 +125,31 @@ def controlFlowForArgs3(args):
 
 
 def parseXML(child):
+    global varTF
+    global varLF
+    global tempframe
+    global localframe
     args = []
     opcode = list(child.attrib.values())[1]
     argumentcount = len(child)
+
+    if opcode == 'CREATEFRAME':
+        varTF = {}
+        tempframe = True
+        return
+
+    if opcode == 'PUSHFRAME':
+        if not tempframe:
+            Error.error_exit("PRISTUP K NEDEFINOVANEMU RAMCI PUSHFRAME!\n", 55)
+        varLF = varTF
+        varTF = {}
+        for item in varLF.keys():
+            if re.match(r"TF@\S", item):
+                string = item.split('@', 1)[1]
+                varLF[f'LF@{string}'] = varLF.pop(f'TF@{string}')
+        localframe = True
+        tempframe = False
+        return
 
     if argumentcount == 3:
         controlFlowForArgs3(child)
@@ -143,16 +167,36 @@ def parseXML(child):
 
 
 def functions(opcode, arg, argumentcount):
+    global varTF
+    global varLF
+    global tempframe
+    global localframe
     argtype = list(arg.attrib.values())[0]
     global index
 
     if opcode == 'DEFVAR':
-        if re.match(r"{}".format(varregex), arg.text):
-            if arg.text in var:
-                Error.error_exit("REDEFINITION OF VARIABLE!\n", 52)
-            var.update({arg.text: ""})
-        else:
+        if not re.match(r"{}".format(varregex), arg.text):
             Error.error_exit("WRONG FORMAT FOR DEFVAR!\n", 53)
+        if arg.text[0:2] == 'LF':
+            if not localframe:
+                Error.error_exit("PRISTUP K NEDEFINOVANEMU RAMCI DEFVAR!\n", 55)
+            if arg.text in varLF:
+                Error.error_exit("REDEFINITION OF VARIABLE!\n", 52)
+            varLF.update({arg.text: ""})
+        elif arg.text[0:2] == 'TF':
+            if not tempframe:
+                Error.error_exit("PRISTUP K NEDEFINOVANEMU RAMCI DEFVAR!\n", 55)
+            if arg.text in varTF:
+                Error.error_exit("REDEFINITION OF VARIABLE!\n", 52)
+            varTF.update({arg.text: ""})
+        elif arg.text in var:
+             Error.error_exit("REDEFINITION OF VARIABLE!\n", 52)
+        if arg.text[0:2] == 'GF':
+            var.update({arg.text: ""})
+        if arg.text[0:2] == 'TF':
+            varTF.update({arg.text: ""})
+        if arg.text[0:2] == 'LF':
+            varLF.update({arg.text: ""})
 
     elif opcode == 'MUL':
         controlRightCountOfArguments(argumentcount, 3)
@@ -206,14 +250,18 @@ def functions(opcode, arg, argumentcount):
         calculate.append("{}@{}".format(argtype, arg.text))
         move()
 
+    elif opcode == 'TYPE':
+        controlRightCountOfArguments(argumentcount, 2)
+        calculate.append("{}@{}".format(argtype, arg.text))
+        typecheck()
+
     else:
         Error.error_exit("UNKNOWN OPCODE!\n", 53)
 
 
-def move():
+def typecheck():
     if len(calculate) < 2:
         return
-
     op1, type1 = calculate[0].split('@', 1)[1], calculate[0].split('@', 1)[0]
     if type1 != 'var':
         Error.error_exit("ZLY ARGUMENT 1! {}\n".format(operator), 53)
@@ -224,15 +272,77 @@ def move():
 
     if not re.match(r"{}".format(varregex), op1):
         Error.error_exit("ZLY ARGUMENT 1! {}\n".format(operator), 53)
+
     if op1 not in var.keys():
-        Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
+        checkIfVarExists(localframe, tempframe, op1)
 
     if re.match(r"{}".format(varregex), op2):
+        checkIfVarExists(localframe, tempframe, op2)
+        if not (var.get(op2) or varLF.get(op2) or varTF.get(op2)):
+            Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
         type2, op2 = variableIsGiven(op2)
 
-    var.update({op1: "{}@{}".format(type2, op2)})
-
+    if op1[0:2] == 'GF':
+        var.update({op1: "string@{}".format(type2)})
+    if op1[0:2] == 'LF':
+        varLF.update({op1: "string@{}".format(type2)})
+    if op1[0:2] == 'TF':
+        varTF.update({op1: "string@{}".format(type2)})
     calculate.clear()
+
+
+def move():
+    if len(calculate) < 2:
+        return
+
+    op1, type1 = calculate[0].split('@', 1)[1], calculate[0].split('@', 1)[0]
+
+    if type1 != 'var':
+        Error.error_exit("ZLY ARGUMENT 1! {}\n".format(operator), 53)
+    if not (re.match(r"{}".format(symbol_regex), calculate[1])):
+        Error.error_exit("ZLY ARGUMENT 2! {}\n".format(operator), 53)
+
+    op2, type2 = calculate[1].split('@', 1)[1], calculate[1].split('@', 1)[0]
+
+    if not re.match(r"{}".format(varregex), op1):
+        Error.error_exit("ZLY ARGUMENT 1! {}\n".format(operator), 53)
+
+    if op1 not in var.keys():
+        checkIfVarExists(localframe, tempframe, op1)
+
+    if re.match(r"{}".format(varregex), op2):
+        checkIfVarExists(localframe, tempframe, op2)
+        if not (var.get(op2) or varLF.get(op2) or varTF.get(op2)):
+            Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
+        type2, op2 = variableIsGiven(op2)
+
+    if op1[0:2] == 'GF':
+        var.update({op1: "{}@{}".format(type2, op2)})
+    if op1[0:2] == 'LF':
+        varLF.update({op1: "{}@{}".format(type2, op2)})
+    if op1[0:2] == 'TF':
+        varTF.update({op1: "{}@{}".format(type2, op2)})
+    calculate.clear()
+
+
+def checkIfVarExists(localframe, tempframe, op1):
+    if op1[0:2] == 'LF' or op1[0:2] == 'TF':
+        if localframe:
+            if op1[0:2] != 'LF' and op1[0:2] != 'GF':
+                Error.error_exit("NEEXISTUJUCI RAMEC! {}\n".format(operator), 55)
+            if op1 not in var.keys() and op1 not in varLF.keys():
+                Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
+            return
+        if tempframe:
+            if op1[0:2] != 'TF' and op1[0:2] != 'GF':
+                Error.error_exit("NEEXISTUJUCI RAMEC! {}\n".format(operator), 55)
+            if op1 not in var.keys() and op1 not in varTF.keys():
+                Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
+            return
+        Error.error_exit("NEEXISTUJUCI RAMEC! {}\n".format(operator), 55)
+    if op1 not in var.keys():
+        Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
+        return
 
 
 def compare(operator):
@@ -254,16 +364,27 @@ def compare(operator):
     if not re.match(r"{}".format(varregex), op1):
         Error.error_exit("ZLY ARGUMENT 1! {}\n".format(operator), 53)
     if op1 not in var.keys():
-        Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
+        checkIfVarExists(localframe, tempframe, op1)
 
     if re.match(r"{}".format(varregex), op2):
+        checkIfVarExists(localframe, tempframe, op2)
+        if not (var.get(op2) or varLF.get(op2) or varTF.get(op2)):
+            Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
         type2, op2 = variableIsGiven(op2)
 
     if re.match(r"{}".format(varregex), op3):
+        checkIfVarExists(localframe, tempframe, op3)
+        if not (var.get(op3) or varLF.get(op3) or varTF.get(op3)):
+            Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
         type3, op3 = variableIsGiven(op3)
 
     result = ops["{}".format(operator)](op2, op3)
-    var.update({op1: "bool@{}".format(str(result).lower())})
+    if op1[0:2] == 'GF':
+        var.update({op1: "bool@{}".format(str(result).lower())})
+    if op1[0:2] == 'LF':
+        varLF.update({op1: "bool@{}".format(str(result).lower())})
+    if op1[0:2] == 'TF':
+        varTF.update({op1: "bool@{}".format(str(result).lower())})
 
     calculate.clear()
     if type2 != type3:
@@ -271,22 +392,34 @@ def compare(operator):
 
 
 def stdoutprint(arg):
-    # TODO NIL@NIL
     if arg == 'nil@nil':
         myprint('')
         return
     if re.match(r"{}".format(varregex), arg):
         if arg not in var.keys():
-            Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
-        testemptyvar = var.get(arg)
+            checkIfVarExists(localframe, tempframe, arg)
+        if (arg[0:2] == 'GF'):
+            testemptyvar = var.get(arg)
+        if (arg[0:2] == 'LF'):
+            testemptyvar = varLF.get(arg)
+        if (arg[0:2] == 'TF'):
+            testemptyvar = varTF.get(arg)
         if not testemptyvar:
             Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
-        myprint(var.get(arg).split('@', 1)[1])
+        if (arg[0:2] == 'GF'):
+            myprint(var.get(arg).split('@', 1)[1])
+        if (arg[0:2] == 'LF'):
+            myprint(varLF.get(arg).split('@', 1)[1])
+        if (arg[0:2] == 'TF'):
+            myprint(varTF.get(arg).split('@', 1)[1])
         return
     myprint(arg)
 
 
 def myprint(string):
+    if string == 'nil':
+        print('', end='')
+        return
     print(string, end='')
 
 
@@ -309,23 +442,35 @@ def arithmetic(operator):
 
     if not re.match(r"{}".format(varregex), op1):
         Error.error_exit("ZLY ARGUMENT 1! {}\n".format(operator), 53)
-    if op1 not in var.keys():
-        Error.error_exit("NEEXISTUJUCA PREMENNA! {}\n".format(operator), 54)
     if not (re.match(r"{}".format(varregex), op2) or re.match(r"[+\-]?\d+", op2)):
         Error.error_exit("ZLY ARGUMENT 2! {}\n".format(operator), 53)
     if not (re.match(r"{}".format(varregex), op3) or re.match(r"[+\-]?\d+", op3)):
         Error.error_exit("ZLY ARGUMENT 3! {}\n".format(operator), 53)
 
+    if op1 not in var.keys():
+         checkIfVarExists(localframe, tempframe, op1)
+
     if re.match(r"{}".format(varregex), op2):
-        type2, op2 = variableIsGiven(op2)
+         checkIfVarExists(localframe, tempframe, op2)
+         if not (var.get(op2) or varLF.get(op2) or varTF.get(op2)):
+             Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
+         type2, op2 = variableIsGiven(op2)
 
     if re.match(r"{}".format(varregex), op3):
+        checkIfVarExists(localframe, tempframe, op3)
+        if not (var.get(op3) or varLF.get(op3) or varTF.get(op3)):
+            Error.error_exit("PREMENNA JE PRAZDNA! {}\n".format(operator), 56)
         type3, op3 = variableIsGiven(op3)
 
     if operator == '/' and int(op3) == 0:
         Error.error_exit("ZERO DIVISION!\n", 57)
     result = ops["{}".format(operator)](int(op2), int(op3))
-    var.update({op1: "int@{}".format(result)})
+    if op1[0:2] == 'GF':
+        var.update({op1: "int@{}".format(str(result).lower())})
+    if op1[0:2] == 'LF':
+        varLF.update({op1: "int@{}".format(str(result).lower())})
+    if op1[0:2] == 'TF':
+        varTF.update({op1: "int@{}".format(str(result).lower())})
 
     calculate.clear()
     if type2 != 'int' or type3 != 'int':
@@ -333,12 +478,22 @@ def arithmetic(operator):
 
 
 def variableIsGiven(op):
-    if op not in var.keys():
+    if not (op in var.keys() or op in varLF.keys() or op in varTF.keys()):
         Error.error_exit("NEEXISTUJUCA PREMENNA!\n", 54)
-    ops = var.get(op)
+    if op[0:2] == 'GF':
+        ops = var.get(op)
+    if op[0:2] == 'LF':
+        ops = varLF.get(op)
+    if op[0:2] == 'TF':
+        ops = varTF.get(op)
     if ops == '':
         Error.error_exit("PREMENNA JE PRAZDNA!\n", 56)
-    types, op = var.get(op).split('@', 1)[0], var.get(op).split('@', 1)[1]
+    if op[0:2] == 'GF':
+        types, op = var.get(op).split('@', 1)[0], var.get(op).split('@', 1)[1]
+    if op[0:2] == 'LF':
+        types, op = varLF.get(op).split('@', 1)[0], varLF.get(op).split('@', 1)[1]
+    if op[0:2] == 'TF':
+        types, op = varTF.get(op).split('@', 1)[0], varTF.get(op).split('@', 1)[1]
 
     return types, op
 
